2df39e41f3b88f3b6801c27a5b0db533e0b9c53e
Juraj <jurajsolensky01@gmail.com>
2026-04-26 20:17:46 +0200

    added instructions for heat error

--- OpenStack/consoleProtocol.sh
+++ OpenStack/consoleProtocol.sh
@@ -10,4 +10,4 @@ juju config nova-cloud-controller console-access-protocol="novnc"
 
 juju status
 echo "*****Wait for ALL units to reach active/idle and you're done.*****"
-echo "*****If heat errors occur, refer the the documentation at docs.cc.uniza.sk for fixing them*****"
+echo "*****If errors related to HEAT occur, refer the the documentation at docs.cc.uniza.sk for fixing them (heat internal error)*****"
